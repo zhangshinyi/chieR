d0890fd942f141ac420429c6ac3bc1553e2a16b7
Kimberly Zhang <kizhang@microsoft.com>
2022-06-05 19:03:56 -0400

    Added new runSQLQuery function

---
+++ R/runSQLQuery.R
@@ -0,0 +1,33 @@
+#' SQL query to run given connection and (table name or full query)
+#' @param conn should be be a connection (e.g., a call of chieR::sqlConnect())
+#' @param table is the table name. Must provide table or query.
+#' @param dateColumn is a column that should be converted to type Date if applicable
+#' @param columns is a vector of column names if you don't want to pull all columns by default
+#' @param query is the full query if you don't want to deal with the other arguments. Must provide table or query.
+#' @keywords query
+#' @export
+#' @examples
+#' runSQLQuery()
+runSQLQuery <- function(conn, table = NULL, dateColumn = NULL, columns = "*", query = NULL){
+  
+  if(is.null(table) & is.null(query)){
+    abort("Must provide either table name or full SQL query")
+  }
+  
+  if(is.null(query)){
+    if(!identical("*", columns)){
+      paste(columns, collapse = ",")
+    }
+    
+    query <- sprintf(paste("SELECT", columns, "FROM %s"), table)
+  }
+  
+  data <- data.table(sqlQuery(conn, query))
+  
+  if(!is.null(dateColumn)){
+    data[, eval(dateColumn) := as.Date(get(dateColumn), "%m/%d/%Y")]
+    setkeyv(data, dateColumn)    
+  }
+  
+  return(data[])
+}
