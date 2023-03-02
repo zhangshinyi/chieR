002c9ef2657bbee1f7527e252d96822f7736ef47
Kimberly Zhang <kizhang@microsoft.com>
2023-03-02 16:14:18 -0500

    Added default colors for processor

--- R/getColors.R
+++ R/getColors.R
@@ -6,25 +6,28 @@
 #' @examples
 #' getColors()
 getColors <- function(type = "Standard"){
-  if(!type %in% c("Standard", "Gen", "HW Bucket", "Capability Gap")){
+  if(!type %in% c("Standard", "Gen", "HW Bucket", "Capability Gap", "Processor")){
     stop("Invalid input for type")
   }
   colors <- switch(type,
                    "Standard" = c("#243A5E", "#0078D4", "#50E6FF", "#8661C5",
-                                  "#D59DFF", "#6A4B16", "#FF9349", "#FFB900",
-                                  "#054B16", "#107C10", "#9BF00B", "#274B47",
-                                  "#008575", "#30E5D0", "#6B2929", "#FEF000",
-                                  "#2F2F2F", "#505050", "#737373", "#D2D2D2",
-                                  "#E6E6E6", "#F2F2F2"),
-                   "Gen"      = c("Gen3"  = "#50E6FF",
-                                  "Gen4"  = "#8661C5",
-                                  "Gen4M" = "#6A4B16",
-                                  "Gen5"  = "#FF9349",
-                                  "Gen6"  = "#FFB900",
-                                  "Gen6M" = "#11444B",
-                                  "Gen7"  = "#FFFB12",
-                                  "Gen7M" = "#6A4B16",
-                                  "Gen8"  = "#287380"),
+                                           "#D59DFF", "#6A4B16", "#FF9349", "#FFB900",
+                                           "#054B16", "#107C10", "#9BF00B", "#274B47",
+                                           "#008575", "#30E5D0", "#6B2929", "#FEF000",
+                                           "#2F2F2F", "#505050", "#737373", "#D2D2D2",
+                                           "#E6E6E6", "#F2F2F2"),
+                                           "Gen"      = c("Gen3"  = "#50E6FF",
+                                                          "Gen4"  = "#8661C5",
+                                                          "Gen4M" = "#6A4B16",
+                                                          "Gen5"  = "#FF9349",
+                                                          "Gen6"  = "#FFB900",
+                                                          "Gen6M" = "#11444B",
+                                                          "Gen7"  = "#FFFB12",
+                                                          "Gen7M" = "#6A4B16",
+                                                          "Gen8"  = "#287380"),
+                   "Processor" = c("Intel" = "#243A5E",
+                                   "AMD"   = "#0078D4",
+                                   "Other" = "#50E6FF"),
                    "HW Bucket" = c("Disk"           = "#243A5E",
                                    "Memory"         = "#0078D4",
                                    "CPU"            = "#50E6FF",
