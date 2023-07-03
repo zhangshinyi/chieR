a97cc1f8b0b2bc1b46e29b3617ba73c5956504c0
Kimberly Zhang <kizhang@microsoft.com>
2023-07-03 18:00:42 -0400

    Added horizontalLegend argument to burndownChart()

--- R/burndownChart.R
+++ R/burndownChart.R
@@ -1,6 +1,7 @@
 #' Wrapper function for burndown plotly charts using Reynold's standard
 #' @param burndownBars is a data table with the bar values; requires the columns Period, lens, and value. Note that the variable column in the example is not required; a column to distinguish incoming/outgoing items is likely helpful (and a prerequisite for any aggregation done prior).
 #' @param backlogLine is a data table with the backlog values for each period; requres the columns Period and Backlog.
+#' @param horizontalLegend is a boolean value indicating whether the legend should be shown horizontally at the top or vertically on the right.
 #' @keywords burndown, plotly, plot
 #' @export
 #' @examples
@@ -26,16 +27,18 @@
 #' totalLine  <- data[, .(Backlog = priorQueue + sum(value)), by = .(Period)]
 #' burndownChart(burndownBars  = data,
 #'               backlogLine  = totalLine)
-burndownChart <- function(burndownBars, backlogLine, legendHeight = 1.05){
+burndownChart <- function(burndownBars, backlogLine, legendHeight = 1.05, horizontalLegend = NULL){
   barTotals <- copy(burndownBars)[value != 0][, sign := sign(value)]
   barTotals <- barTotals[, .(value = sum(value)), by = .(Period, sign)]
   barTotals[, `:=`(text, format(round(value, 0), nsmall = 0, big.mark = ","))]
+
+  numLensVals <- length(unique(burndownBars$lens))
 
   plot <- plot_ly() %>%
     add_bars(x      = ~burndownBars$Period,
              y      = ~burndownBars$value,
              color  = ~burndownBars$lens,
-             colors = chieR::getColors()[1:length(unique(burndownBars$lens))])  %>%
+             colors = chieR::getColors()[1:numLensVals])  %>%
     layout(barmode = "relative",
            xaxis   = list(title     = "",
                           tickangle = 320),
@@ -58,5 +61,8 @@ burndownChart <- function(burndownBars, backlogLine, legendHeight = 1.05){
                     opacity   = 0.85,
                     font      = list(family = chieR::getFont("Standard"),
                                      size  = chieR::getFontSize("annotation")))
-  chieR::plotlyLayout(plot, legendHeight = legendHeight)
+  if(is.null(horizontalLegend)){
+    horizontalLegend <- ifelse(numLensVals < 8, TRUE, FALSE)
+  }
+  chieR::plotlyLayout(plot, legendHeight = legendHeight, horizontalLegend = horizontalLegend)
 }
