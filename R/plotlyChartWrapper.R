2f0d58c340940ad11b707e7ecee8ebd685935a97
Kimberly Zhang <kizhang@microsoft.com>
2023-07-11 14:12:41 -0400

    Add greyscale for cases where there are too many lens values

--- R/plotlyChartWrapper.R
+++ R/plotlyChartWrapper.R
@@ -62,8 +62,16 @@ plotlyChartWrapper <- function(data,
     type <- NULL
   }
   lensValues <- sort(unique(data$lens))
-  lensColors <- chieR::getColors(colorPalette)[1:length(lensValues)]
+  lensColors <- chieR::getColors(colorPalette)
+  if(length(lensValues) <= length(lensColors)){
+    lensColors <- lensColors[1:length(lensValues)]
+  } else {
+    lensColors <- lensColors[!lensColors %in% c("#2F2F2F", "#505050", "#737373", "#D2D2D2", "#E6E6E6", "#F2F2F2")]
+    lensColors <- c(lensColors, gray.colors(length(lensValues) - length(lensColors)))
+  }
+
   names(lensColors) <- lensValues
+
   output <- plot_ly(data, x = ~Period, y = ~value, color = ~lens, source = sourceName,
                     colors = lensColors, type = type, mode = mode) %>% plotly::layout(barmode = barmode,
                                                                                       legend = list(traceorder = "normal"))
