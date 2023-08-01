2fd3fc338292668852be99155f03e71652a68b61
Kimberly Zhang <kizhang@microsoft.com>
2023-08-01 05:05:27 -0400

    Added arguments for agility standard

--- R/agilityStandard.R
+++ R/agilityStandard.R
@@ -5,25 +5,39 @@
 #' @keywords filters
 #' @export
 #' @examples
-agilityStandard <- function(input, output, session, agilityData, keyword){ 
-  
+# agilityStandard <- function(input, output, session, agilityData, keyword,
+agilityStandard <- function(input, output, session, agilityData, keyword,
+                            boxTitle                     = "Agility",
+                            agilityActionButtonGroupList = NULL,
+                            actionButtonInputFunction    = NULL){
+
+  agilityWithActionButtonFilter <- reactive({
+    if(!is.null(actionButtonInputFunction) & !is.null(agilityActionButtonGroupList)){
+      data <- actionButtonInputFunction(data = copy(agilityData$data), inputValue = input[[agilityActionButtonGroupList$inputId]])
+    } else {
+      data <- copy(agilityData$data)
+    }
+    data
+  })
+
   ###################################################################################### UI, Widgets
-  
+
   filterCols <- reactive({
     filters <- agilityData$filterCols
     data.table(col       = filters,
                inputName = paste0(keyword, gsub(" ", "", filters)))
   })
-  
+
   output[[paste0("set", keyword, "DateRange")]] <- renderUI({
-    shiny::req(agilityData$data)
-    
-    dates <- copy(agilityData$data)$Date
-    
+    shiny::req(agilityWithActionButtonFilter())
+
+    dates <- copy(agilityWithActionButtonFilter())$Date
+
     minDate   <- min(dates)
     maxDate   <- max(dates)
-    
-    dateRangeInput(paste0(keyword, "DateRange"), 
+
+    # div(id = paste0(keyword, "DateRange", input$resetRedlineInput),
+    dateRangeInput(paste0(keyword, "DateRange"),
                    label = "Date Range",
                    min   = minDate,
                    start = max(c(seq(min(agilityData$semesterDates$Date), length = 2, by = "-3 months")[2], minDate)),
@@ -32,17 +46,17 @@ agilityStandard <- function(input, output, session, agilityData, keyword){
                    width = "100%")
     #)
   })
-  
+
   output[[paste0("resetable", keyword, "Input")]] <- renderUI({
-    shiny::req(agilityData$data, filterCols())
+    shiny::req(agilityWithActionButtonFilter(), filterCols())
     filterWidgets <- mapply(function(csvColName, inputName){
-      inputChoices <- as.character(sort(unique(agilityData$data[[csvColName]])))
+      inputChoices <- as.character(sort(unique(agilityWithActionButtonFilter()[[csvColName]])))
       column(width = 2,
              pickerInput(inputName,
-                         csvColName, 
+                         csvColName,
                          selected = inputChoices,
                          choices  = inputChoices,
-                         options  = list(`actions-box`          = TRUE, 
+                         options  = list(`actions-box`          = TRUE,
                                          `live-search`          = TRUE,
                                          `selected-text-format` = paste0("count > ", length(inputChoices) - 1),
                                          `count-selected-text`  = "All"),
@@ -51,11 +65,11 @@ agilityStandard <- function(input, output, session, agilityData, keyword){
     csvColName = filterCols()$col,
     inputName  = filterCols()$inputName,
     SIMPLIFY   = FALSE)
-    
+
     fixedWidgets <- list(column(width = 2,
                                 pickerInput(paste0(keyword, "TimeInterval"),
                                             label    = "Time Interval",
-                                            choices  = c("Week", "Month"), 
+                                            choices  = c("Week", "Month"),
                                             selected = "Month",
                                             multiple = FALSE)),
                          column(width = 2,
@@ -63,34 +77,67 @@ agilityStandard <- function(input, output, session, agilityData, keyword){
                          column(width = 2,
                                 pickerInput(paste0(keyword, "Lens"),
                                             label    = "Lens",
-                                            choices  = "None", 
+                                            choices  = "None",
                                             selected = "None",
                                             multiple = FALSE)))
-    
+
     div(id = paste0(keyword, input[[paste0("reset", keyword, "Input")]]),
         chieR::organizeWidgets(filters = filterWidgets, dateLensWidgets = fixedWidgets, 3))
   })
-  
+
+  output[[paste0(keyword, "UI")]] <- renderUI({
+    if(!is.null(agilityActionButtonGroupList)){
+      agilityActionButtonGroup <- radioGroupButtons(inputId  = agilityActionButtonGroupList$inputId,
+                                                    label    = NULL,
+                                                    choices  = agilityActionButtonGroupList$choices,
+                                                    status   = "primary")
+    } else {
+      agilityActionButtonGroup <- NULL
+    }
+    list(fluidRow(column(width = 12,
+                         div(style = "display:inline-block; float:right",
+                             tipify(actionButton(paste0("reset", keyword, "Input"),
+                                                 "",
+                                                 icon = icon("redo")),
+                                    "Refresh settings",
+                                    placement = "top"))),
+                  column(width = 12,
+                         uiOutput(paste0("resetable", keyword, "Input")))
+    ),
+    fluidRow(
+      column(width = 12,
+             box(width       = NULL,
+                 title       = boxTitle,
+                 status      = "primary",
+                 solidHeader = TRUE,
+                 agilityActionButtonGroup,
+                 plotlyOutput(paste0(keyword, "Plot"),
+                              height = "590px"),
+                 # reactableOutput("agilityTable"),
+                 align = "center")
+      )))
+  })
+
   ###################################################################################### Calcs
-  
+
   # Filter based on UI widgets
   agilityFiltered <- reactive({
-    shiny::req(agilityData$data, 
+    shiny::req(agilityWithActionButtonFilter(),
                input[[paste0(keyword, "DateRange")]])
-    
-    data  <- copy(agilityData$data)
-    
+
+    data  <- copy(agilityWithActionButtonFilter())
+
     for(i in 1:nrow(filterCols())){
       shiny::req(input[[filterCols()[i]$inputName]])
       data <- data[get(filterCols()[i]$col) %in% input[[filterCols()[i]$inputName]]]
     }
-    
+
     # Filter by date range
     data <- data[(Date >= min(input[[paste0(keyword, "DateRange")]])) & (Date <= max(input[[paste0(keyword, "DateRange")]]))]
-    
+
     data
   })
-  
+
   agilityPeriodMapping <- reactive({
     shiny::req(input[[paste0(keyword, "TimeInterval")]])
     periodCol   <- paste0("formatted", input[[paste0(keyword, "TimeInterval")]])
@@ -98,46 +145,46 @@ agilityStandard <- function(input, output, session, agilityData, keyword){
     setnames(dateMapping, periodCol, "Period")
     dateMapping
   })
-  
+
   agilityWithPeriod <- reactive({
     shiny::req(agilityFiltered(), agilityPeriodMapping())
     merge(copy(agilityFiltered()), agilityPeriodMapping(), by = "Date", all.x = TRUE)
   })
-  
+
   agilityAggregated <- reactive({
-    shiny::req(agilityWithPeriod(), nrow(agilityWithPeriod()) > 0, 
+    shiny::req(agilityWithPeriod(), nrow(agilityWithPeriod()) > 0,
                input[[paste0(keyword, "DateRange")]],
                agilityPeriodMapping())
-    
+
     data <- copy(agilityWithPeriod())[, .(Period, value)]
-    
+
     # Fill in missing periods if needed
-    missingPeriods  <- unique(agilityPeriodMapping()[(Date >= min(input[[paste0(keyword, "DateRange")]])) & 
-                                                       (Date <= max(input[[paste0(keyword, "DateRange")]])) & 
+    missingPeriods  <- unique(agilityPeriodMapping()[(Date >= min(input[[paste0(keyword, "DateRange")]])) &
+                                                       (Date <= max(input[[paste0(keyword, "DateRange")]])) &
                                                        !(Period %in% data$Period)]$Period)
     if(length(missingPeriods) > 0){
       data <- rbind(data,
                     data.table(Period = missingPeriods,
                                value  = -100))
     }
-    
+
     data
   })
-  
+
   ###################################################################################### Plot
-  
+
   output[[paste0(keyword, "Plot")]] <- renderPlotly({
     shiny::req(agilityAggregated(), nrow(agilityAggregated()) > 0)
-    chieR::agilityPlot(boxplotData     = copy(agilityAggregated()), 
+    chieR::agilityPlot(boxplotData     = copy(agilityAggregated()),
                        plotlyRangemode = "nonnegative") %>%
       layout(margin = list(l = 0))
   })
-  
+
   ###################################################################################### Table
-  
+
   agility            <- reactiveValues()
   agility$clickTable <- FALSE
-  
+
   # If user clicks, filter more as appropriate
   observeEvent(event_data("plotly_click", source = paste0(keyword, "Plot")), {
     shiny::req(agilityWithPeriod())
@@ -145,25 +192,25 @@ agilityStandard <- function(input, output, session, agilityData, keyword){
     clickInfo          <- event_data("plotly_click", source = paste0(keyword, "Plot"))
     agility$tableData  <- copy(agilityWithPeriod())[Period %in% clickInfo$x]
   })
-  
+
   # If user clicks, filter more as appropriate
-  observeEvent(input$resetagilityInput, {
+  observeEvent(input[[paste0("reset", keyword, "Input")]], {
     agility$clickTable <- FALSE
   })
-  
+
   output[[paste0(keyword, "Table")]] <- renderReactable({
-    shiny::req(agilityWithPeriod(), nrow(agilityWithPeriod()) > 0, 
+    shiny::req(agilityWithPeriod(), nrow(agilityWithPeriod()) > 0,
                is.logical(agility$clickTable))
-    
+
     if(!agility$clickTable){
       tableData <- copy(agilityWithPeriod())
     } else {
       tableData <- copy(agility$tableData)
     }
-    
+
     # Add HTML for URL
     tableData[, Id := paste0('<a href="', Url, '" target="_blank" rel="noopener noreferrer">', Id,'</a>')]
-    
+
     tableData <- tableData[, c("Id",
                                "Start Date",
                                "End Date",
@@ -174,16 +221,16 @@ agilityStandard <- function(input, output, session, agilityData, keyword){
                                "ClusterSize",
                                "value",
                                "SLA"), with = FALSE]
-    setnames(tableData, 
+    setnames(tableData,
              c("ClusterSize",  "DevModel",  "value"),
              c("Cluster Size", "Dev Model", "Duration"))
-    
+
     tableData <- tableData[!is.na(Duration)]
-    
+
     tableData[, `SLA Met` := ifelse(Duration <= SLA, "Yes", "No")]
-    
+
     setkey(tableData, Id)
-    
+
     reactable(tableData,
               defaultColDef   = colDef(vAlign      = "center",
                                        header      = function(value){ gsub(".", " ", value, fixed = TRUE) },
@@ -195,12 +242,12 @@ agilityStandard <- function(input, output, session, agilityData, keyword){
               columns         = list(`Title`   = colDef(width = 500),
                                      `SLA Met` = colDef(
                                        cell = function(value) {
-                                         if(value  == "No") shiny::icon("times-circle", class = "fas",  
-                                                                        style = "color: #D83B01") else shiny::icon("check-circle", class = "fas",  
+                                         if(value  == "No") shiny::icon("times-circle", class = "fas",
+                                                                        style = "color: #D83B01") else shiny::icon("check-circle", class = "fas",
                                                                                                                    style = "color: #107C10")
                                        }
                                      )),
-              striped         = TRUE, 
+              striped         = TRUE,
               bordered        = TRUE,
               highlight       = TRUE)
   })
